0d3b6d351853973c4619ac1f5b3397bb448490b2
Lita <litaionutm9@gmail.com>
2025-12-14 21:08:15 +0200

    admin: show waitlist emails in Django admin

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WaitlistSignup
 
-# Register your models here.
+@admin.register(WaitlistSignup)
+class WaitlistSignupAdmin(admin.ModelAdmin):
+    list_display = ("email", "created_at")
+    search_fields = ("email",)
+    ordering = ("-created_at",)
